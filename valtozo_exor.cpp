0956be59888f169c9bcc8e93e50bbf3d4293da84
Salesz9902 <35422187+Salesz9902@users.noreply.github.com>
2018-09-27 21:27:44 +0200

    Create valtozo_exor.cpp

---
+++ valtozo_exor.cpp
@@ -0,0 +1,16 @@
+#include <std_lib_facilities.h>
+
+int main()
+{
+int a = 2, b = 13;
+
+cout << "csere elott: a = " << a << " b = " << b << '\n';
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+
+cout << "csere utan: a = " << a << " b = " << b << '\n';
+
+return 0;
+}
